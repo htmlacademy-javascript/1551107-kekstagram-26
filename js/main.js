03432d3f38a51220a9402f62d824458a3c8d7efb
Grigoriy <grifa111@yandex.ru>
2022-06-12 20:44:08 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomNumber(min, max) {
+const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min >= max) {
@@ -9,14 +9,9 @@ function getRandomNumber(min, max) {
     throw Error('Ошибка поиска');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const checkStringLength = (string, maxLength) => {
-  if(string.maxLength <= maxLength) {
-    return true;
-  }
-  return false;
 };
+
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 getRandomNumber();
 checkStringLength();
